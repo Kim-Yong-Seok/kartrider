830e576ef210e10de1356c287ba30665b4fa4170
Kim-Yong-Seok <tjrdydrla202@naver.com>
2021-02-22 10:25:36 +0900

    2021.02.25 backup

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/index.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <p className="title">KARTRIDER</p>
+        </div>
+    );
+}
+
+export default Header;
