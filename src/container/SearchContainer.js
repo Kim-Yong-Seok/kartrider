5307450242bc3b2f4cb4d7d7b2b035d1c3dd5012
Kim-Yong-Seok <tjrdydrla202@naver.com>
2021-03-11 17:59:12 +0900

    2021 03 11 backup

--- src/container/SearchContainer.js
+++ src/container/SearchContainer.js
@@ -4,9 +4,22 @@ import axios from 'axios';
 import Search from '../page/Search';
 
 function SearchContainer( props ) {
-    const userId = props.location.id;
+    const userId = props.location.id || '용석짱짱121';
     const [ matches, setMatches ] = useState([]);
-    
+    const [ winRate, setWinRate ] = useState();
+    const [ retire, setRetire ] = useState();
+    const [ detailTab, setDetailTab ] = useState([]);
+    const [ limit, setLimit ] = useState(10);
+
+    const searchMatchDetails = async ( matchId ) => {
+        const header = {
+            headers: {
+                Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiNzM4NjMzMjE1IiwiYXV0aF9pZCI6IjIiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4iLCJzZXJ2aWNlX2lkIjoiNDMwMDExMzkzIiwiWC1BcHAtUmF0ZS1MaW1pdCI6IjUwMDoxMCIsIm5iZiI6MTYxNDkyOTM1NCwiZXhwIjoxNjMwNDgxMzU0LCJpYXQiOjE2MTQ5MjkzNTR9.njyfxqNCxFjACrhNHRAfDuj00MSyGIOTjqDp0YjybYs'
+            }
+        };
+        const res = await axios.get('https://api.nexon.co.kr/kart/v1.0/matches/' + matchId, header);
+        return res;
+    }
 
     useEffect(() => {
         const getUserData = async () => {
@@ -16,16 +29,46 @@ function SearchContainer( props ) {
                 }
             };
             const user = await axios.get('https://api.nexon.co.kr/kart/v1.0/users/nickname/'+ userId, header);
-            const match = await axios.get('https://api.nexon.co.kr/kart/v1.0/users/'+ user.data.accessId +'/matches', header);
-            setMatches( match.data.matches[0].matches );
+            const match = await axios.get('https://api.nexon.co.kr/kart/v1.0/users/'+ user.data.accessId +'/matches?limit=' + limit, header);
+            const mat = match.data.matches[0].matches;
+            
+            console.log( mat );
+            let win = 0, ret = 0;
+            let detail = [];
+
+            mat.map((e, idx) => {
+                e.matchResult === '1' && win++;
+                e.player.matchRank === '99' && ret++;
+                detail[idx] = {
+                    idx: idx,
+                    value: 'off'
+                };
+                return true;
+            });
+
+            setDetailTab( detail );
+            setWinRate( win );
+            setRetire( ret );
+            setMatches( mat );
         }
         getUserData();
-    }, []);
+        console.log('user ID is changed!!');
+    }, [userId, limit]);
+
+    useEffect(() => {
+        console.log( detailTab );
+    }, [detailTab])
 
     return (
         <Search 
             matches = { matches }
             userName = { userId }
+            winRate = { winRate }
+            retire = { retire }
+            searchMatchDetails = { searchMatchDetails }
+            detailTab = { detailTab }
+            setDetailTab = { setDetailTab }
+            setLimit = { setLimit } 
         />
     )
 }
--- src/page/Search.js
+++ src/page/Search.js
@@ -2,10 +2,76 @@ import React from 'react';
 import Header from '../components/Header';
 import tracks from '../metadata/track.json';
 import karts from '../metadata/kart.json';
+import image from '../metadata/kart/d47aa62de79d88ecee263e07456555d99ff8957f1760d0f248667913acbc2b67.png';
 
-function MatchList({ match }) {
-    // console.log( match );
-    const { playerCount, player, matchResult, startTime, trackId } = match;
+function MatchDetail({ data, userName }) {
+    console.log( data );
+
+    const getUsers = ( teams ) => {
+
+        console.log( teams );
+        let users = [];
+        let cnt = 0;
+        
+        teams[0].players.map( player => {
+            if( player.characterName && player.matchRank ) {
+                users[cnt] = {
+                    characterName: player.characterName,
+                    matchRank: player.matchRank,
+                    team: 'red',
+                    kart: player.kart
+                }
+                cnt++;
+            }
+            return true;
+        });
+        teams[1].players.map( player => {
+            if( player.characterName && player.matchRank ) {
+                users[cnt] = {
+                    characterName: player.characterName,
+                    matchRank: player.matchRank,
+                    team: 'blue',
+                    kart: player.kart
+                }
+                cnt++;
+            }
+            return true;
+        });
+        console.log( users );
+        users.sort(( a, b ) => {
+            return a.matchRank < b.matchRank ? -1 : a.matchRank > b.matchRank ? 1 : 0;
+        })
+        console.log( users );
+
+        return users;
+    }
+    
+    const userData = getUsers( data.data.teams );
+
+    return (
+        <React.Fragment>
+            {
+                userData.map( ( player ) => {
+                    return (
+                        <div className={ `match-detail-list ${ userName === player.characterName ? 'me' : ''}` } key={ player.characterName }>
+                            <p className={`rank-number ${ player.team }`}>{ player.matchRank === '99' ? '-' : player.matchRank }</p>
+                            <div className="user-area">
+                                <p className="user">{ player.characterName }</p>
+                                {/* <p className="user-record">1'31'333</p> */}
+                            </div>
+                            <img src="/kart.png" className="kart-img" alt="kart-img" />
+                        </div>
+                    )
+                } )
+            }
+            
+        </React.Fragment>
+    )
+}
+
+
+function MatchList({ match, key, searchMatchDetails, setDetailTab, detailTab, idx, userName }) {
+    const { playerCount, player, matchResult, startTime, trackId, matchId } = match;
     const { matchRank, matchTime, kart } = player;
 
     function msToTime(s) {
@@ -23,7 +89,7 @@ function MatchList({ match }) {
       
         return pad(mins) + "'" + pad(secs) + "'" + pad(ms, 3);
     }
-
+    
     const curruntDate = new Date();
     const startDate = new Date( startTime );
     const dateDiff = Math.floor((curruntDate - startDate) / (1000*60*60*24));
@@ -33,12 +99,19 @@ function MatchList({ match }) {
     const kartName = karts.find( e => {
         return e.id === kart;
     });
-    const kartImg = '../metadata/kart/' + kart + '.png';
+    // const kartImg = '../metadata/kart/' + kart + '.png';
     const retire = matchRank === "99" ? 'retire' : matchRank + '/' + playerCount;
 
-    console.log( kartImg );
     return (
-        <div className="match-list">
+        <React.Fragment>
+        <div className="match-list" onClick={() => {
+            detailTab[idx].value === 'off' ?
+            searchMatchDetails( matchId ).then(( res ) => {
+                setDetailTab(detailTab.map( item => item.idx === idx ? { ...item, value: item.value === 'off' ? 'on' : 'off', data: res } : item ));
+            })
+            :
+            setDetailTab(detailTab.map( item => item.idx === idx ? { ...item, value: item.value === 'off' ? 'on' : 'off' } : item ));
+        }}>
             <div className={`aside-color ${matchResult === "1" ? 'win' : 'lose'}`}></div>
             <div className="match-info">
                 <p className="date">{ dateDiff }일전</p>
@@ -53,18 +126,20 @@ function MatchList({ match }) {
                 <p className="record">{ msToTime( matchTime ) }</p>
             </div>
             <div className="div1"></div>
-            <img src={require('../metadata/kart/d47aa62de79d88ecee263e07456555d99ff8957f1760d0f248667913acbc2b67.png')} className="kart-img" alt="kart-img" onError={ e => {
+            <img src={ image } className="kart-img" alt="kart-img" onError={ e => {
                 e.target.onError = null;
                 e.target.src = '/kart.png';
             } }/>
             <p className="kart-name">{ kartName ? kartName.name : '알 수 없는 카트' }</p>
-            <img src='../metadata/kart/d47aa62de79d88ecee263e07456555d99ff8957f1760d0f248667913acbc2b67.png' alt="??" />
         </div>
+        {
+            detailTab && detailTab[idx].value === 'on' && <MatchDetail data={ detailTab[idx].data } userName={ userName } />
+        }
+        </React.Fragment>
     )
 }
 
-function Search({ userName, matches }) {
-    console.log( matches );
+function Search({ userName, matches, winRate, retire, searchMatchDetails, detailTab, setDetailTab }) {
     return (
         <div className="container">
             <Header searchBtn={ true } />
@@ -110,7 +185,7 @@ function Search({ userName, matches }) {
                         <p className="sub-title">승률</p>
                         <div className="graph">
                             <div className="graph-border">
-                                50%
+                                { parseInt( (winRate / matches.length) * 100) }%
                             </div>
                         </div>
                     </div>
@@ -118,7 +193,7 @@ function Search({ userName, matches }) {
                         <p className="sub-title">완주율</p>
                         <div className="graph">
                             <div className="graph-border">
-                                50%
+                                { 100 - parseInt( (retire / matches.length) * 100) }%
                             </div>
                         </div>
                     </div>
@@ -126,7 +201,7 @@ function Search({ userName, matches }) {
                         <p className="sub-title">리타이어율</p>
                         <div className="graph">
                             <div className="graph-border">
-                                50%
+                                { parseInt( (retire / matches.length) * 100) }%
                             </div>
                         </div>
                     </div>
@@ -134,79 +209,19 @@ function Search({ userName, matches }) {
             </div>
             <div className="search-section4">
                 {
-                    matches.map( match => {
-                        return <MatchList match={ match } />
+                    matches.map( (match, index) => {
+                        return (
+                        <MatchList
+                            match={ match }
+                            idx={ index }
+                            key={ match.matchId }
+                            searchMatchDetails={ searchMatchDetails }
+                            setDetailTab={ setDetailTab }
+                            detailTab={ detailTab }
+                            userName={ userName }
+                        />);
                     })
                 }
-                <div className="match-list">
-                    <div className="aside-color win"></div>
-                    <div className="match-info">
-                        <p className="date">4일전</p>
-                        <p className="map">MAP</p>
-                        <p className="win-or-lose">승리</p>
-                        <p className="record">기록</p>
-                    </div>
-                    <div className="match-detail">
-                        <p className="map">차이나 황산</p>
-                        <p className="rank">#2/3</p>
-                        <div className="div"></div>
-                        <p className="record">1'35'12</p>
-                    </div>
-                    <div className="div1"></div>
-                    <img src="/kart.png" className="kart-img" alt="kart-img" />
-                    <p className="kart-name">흑기사X</p>
-                </div>
-                <div className="match-detail-list">
-                    <img src="/1.png" className="rank-number" alt="kart-img" />
-                    <div className="user-area">
-                        <p className="user">Yongs</p>
-                        <p className="user-record">1'31'333</p>
-                    </div>
-                    <img src="/kart.png" className="kart-img" alt="kart-img" />
-                </div>
-                <div className="match-detail-list">
-                    <img src="/2.png" className="rank-number" alt="kart-img" />
-                    <div className="user-area">
-                        <p className="user">Yongs</p>
-                        <p className="user-record">1'31'333</p>
-                    </div>
-                    <img src="/kart.png" className="kart-img" alt="kart-img" />
-                </div>
-                <div className="match-detail-list">
-                    <img src="/3.png" className="rank-number" alt="kart-img" />
-                    <div className="user-area">
-                        <p className="user">Yongs</p>
-                        <p className="user-record">1'31'333</p>
-                    </div>
-                    <img src="/kart.png" className="kart-img" alt="kart-img" />
-                </div>
-                <div className="match-detail-list">
-                    <img src="/4.png" className="rank-number" alt="kart-img" />
-                    <div className="user-area">
-                        <p className="user">Yongs</p>
-                        <p className="user-record">1'31'333</p>
-                    </div>
-                    <img src="/kart.png" className="kart-img" alt="kart-img" />
-                </div>
-                
-                <div className="match-list">
-                    <div className="aside-color win"></div>
-                    <div className="match-info">
-                        <p className="date">4일전</p>
-                        <p className="map">MAP</p>
-                        <p className="win-or-lose">승리</p>
-                        <p className="record">기록</p>
-                    </div>
-                    <div className="match-detail">
-                        <p className="map">차이나 황산</p>
-                        <p className="rank">#2/3</p>
-                        <div className="div"></div>
-                        <p className="record">1'35'12</p>
-                    </div>
-                    <div className="div1"></div>
-                    <img src="/kart.png" className="kart-img" alt="kart-img" />
-                    <p className="kart-name">흑기사X</p>
-                </div>
             </div>
         </div>
     );
